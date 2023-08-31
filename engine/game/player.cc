vitawrap/tge14

commit 4b9d0c50b69ce91aca89d16bd8d03ba5ff0a4ae4

Scene: Render first person ShapeBase in mirrors

engine/game/player.cc
@@ -1272,7 +1272,7 @@ void Player::interpolateTick(F32 dt)
    // apply camera effects - is this the best place? - bramage
    // nope, THIS is the best place - viwrap
    GameConnection* connection = GameConnection::getConnectionToServer();
-   if (connection->isFirstPerson())
+   if (isFirstPerson())
    {
        ShapeBase* obj = connection->getControlObject();
        if (obj == this)
@@ -1499,7 +1499,7 @@ void Player::updateMove(const Move* move)
       if (y > M_PI) y -= M_2PI;
 
       GameConnection* con = getControllingClient();
-      if (move->freeLook && ((isMounted() && getMountNode() == 0) || (con && !con->isFirstPerson())))
+      if (move->freeLook && ((isMounted() && getMountNode() == 0) || (con && !isFirstPerson())))
       {
          mHead.z = mClampF(mHead.z + y,
                            -mDataBlock->maxFreelookAngle,
@@ -4035,13 +4035,13 @@ void Player::renderMountedImage(SceneState* state, ShapeImageRenderImage* rimage
    AssertFatal(rimage->mSBase == this, "Error, wrong image");
    GameConnection *con = GameConnection::getConnectionToServer();
    bool renderMounts = true;
-   if(con && con->getControlObject() == this && con->isFirstPerson())
+   if(con && isFirstPerson())
       renderMounts = sRenderMyItems;
    if (renderMounts == false)
       return;
 
    bool fogExemption = false;
-   if(con && con->getControlObject() == this && con->isFirstPerson() == true)
+   if(con && isFirstPerson() == true)
       fogExemption = true;
    F32 fogAmount = 0.0f;
    if (fogExemption == false)
@@ -4119,7 +4119,7 @@ void Player::renderImage(SceneState* state, SceneRenderImage* image)
    // Decide whether we are going to render the player shape or not
    bool renderPlayer = true;
    bool renderMounts = true;
-   if (con && con->getControlObject() == this && con->isFirstPerson()) {
+   if (con && isFirstPerson()) {
       renderPlayer = mDataBlock->renderFirstPerson;
 
       // Options that let the client turn off (but not on) rendering
@@ -4169,7 +4169,7 @@ void Player::renderImage(SceneState* state, SceneRenderImage* image)
 
       bool fogExemption = false;
       GameConnection *con = GameConnection::getConnectionToServer();
-      if(con && con->getControlObject() == this && con->isFirstPerson() == true)
+      if(con && isFirstPerson() == true)
          fogExemption = true;
       fogAmount = fogExemption ? 0.0 : state->getHazeAndFog(dist,cameraOffset.z);
 

engine/game/shapeBase.cc
@@ -34,6 +34,7 @@ IMPLEMENT_CO_DATABLOCK_V1(ShapeBaseData);
 //----------------------------------------------------------------------------
 // Timeout for non-looping sounds on a channel
 static SimTime sAudioTimeout = 500;
+bool ShapeBase::gRenderFirstPersonShapeOverride = false;
 bool ShapeBase::gRenderEnvMaps = true;
 F32  ShapeBase::sWhiteoutDec = 0.007;
 F32  ShapeBase::sDamageFlashDec = 0.007;
@@ -1073,7 +1074,7 @@ bool ShapeBase::isFirstPerson()
       return true;
 
    if (GameConnection* con = getControllingClient())
-      return con->getControlObject() == this && con->isFirstPerson();
+      return con->getControlObject() == this && (con->isFirstPerson() && !gRenderFirstPersonShapeOverride);
    return false;
 }
 

engine/game/shapeBase.h
@@ -988,6 +988,7 @@ public:
 
    /// @}
 
+   static bool gRenderFirstPersonShapeOverride;
    static bool gRenderEnvMaps; ///< Global flag which turns on or off all environment maps
    static F32  sWhiteoutDec;
    static F32  sDamageFlashDec;

engine/interior/mirrorSubObject.cc
@@ -414,7 +414,7 @@ SubObjectRenderImage* MirrorSubObject::getRenderImage(SceneState*    state,
    getSOTransform().mulP(startPoint);
    startPoint.convolve(getSOScale());
 
-   state->insertTransformPortal(this, 0, realZone, startPoint, true);
+   state->insertTransformPortal(this, 0, realZone, startPoint, true, true);
 
    return ri;
 }

engine/sceneGraph/sceneState.cc
@@ -10,6 +10,7 @@
 #include "sceneGraph/sceneGraph.h"
 #include "terrain/sky.h"
 #include "platform/profiler.h"
+#include "game/shapeBase.h"
 
 namespace {
 
@@ -209,6 +210,7 @@ SceneState::SceneState(SceneState*    parent,
    setupFog();
 
    mTerrainOverride = false;
+   mRenderFirstPersonShapeOverride = false;
 
    mEnvironmentMap  = envMap;
 
@@ -277,7 +279,7 @@ void SceneState::insertRenderImage(SceneRenderImage* ri)
 
 void SceneState::insertTransformPortal(SceneObject* owner, U32 portalIndex,
                                        U32 globalZone,     const Point3F& traversalStartPoint,
-                                       const bool flipCull)
+                                       const bool flipCull, const bool renderFirstPersonShape)
 {
    mTransformPortals.increment();
    mTransformPortals.last().owner         = owner;
@@ -285,6 +287,7 @@ void SceneState::insertTransformPortal(SceneObject* owner, U32 portalIndex,
    mTransformPortals.last().globalZone    = globalZone;
    mTransformPortals.last().traverseStart = traversalStartPoint;
    mTransformPortals.last().flipCull      = flipCull;
+   mTransformPortals.last().renderFirstPersonShape = renderFirstPersonShape;
 }
 
 void SceneState::sortRenderImages()
@@ -465,6 +468,9 @@ void SceneState::renderCurrentImages()
    glPushMatrix();
    dglLoadMatrix(&mModelview);
 
+   if (mRenderFirstPersonShapeOverride)
+       ShapeBase::gRenderFirstPersonShapeOverride = true;
+
    U32 i;
    for (i = 0; i < mRenderImages.size(); i++)
       renderImage(this, mRenderImages[i]);
@@ -476,6 +482,8 @@ void SceneState::renderCurrentImages()
 
    for (i = 0; i < mTranslucentEndImages.size(); i++)
       renderImage(this, mTranslucentEndImages[i]);
+
+   ShapeBase::gRenderFirstPersonShapeOverride = false;
 
    glMatrixMode(GL_MODELVIEW);
    glPopMatrix();

engine/sceneGraph/sceneState.h
@@ -149,6 +149,7 @@ class SceneState
       U32          globalZone;
       Point3F      traverseStart;
       bool         flipCull;
+      bool         renderFirstPersonShape;
    };
 
   public:
@@ -227,20 +228,27 @@ class SceneState
 
    /// Adds a new transform portal to the SceneState.
    ///
-   /// @param   owner                 SceneObject owner of the portal (portalized object).
-   /// @param   portalIndex           Index of the portal in the list of portal planes.
-   /// @param   globalZone            Index of the zone this portal is in in the list of ZoneStates.
-   /// @param   traversalStartPoint   Start point of the zone traversal.
-   ///                                @see SceneGraph::buildSceneTree
-   ///                                @see SceneGraph::findZone
+   /// @param   owner                   SceneObject owner of the portal (portalized object).
+   /// @param   portalIndex             Index of the portal in the list of portal planes.
+   /// @param   globalZone              Index of the zone this portal is in in the list of ZoneStates.
+   /// @param   traversalStartPoint     Start point of the zone traversal.
+   ///                                  @see SceneGraph::buildSceneTree
+   ///                                  @see SceneGraph::findZone
+   /// @param   renderFirstPersonShape  Force rendering of the control object shape currently in first person.
    ///
    /// @param   flipCull              If true, the portal plane will be flipped
    void insertTransformPortal(SceneObject* owner, U32 portalIndex,
                               U32 globalZone,     const Point3F& traversalStartPoint,
-                              const bool flipCull);
+                              const bool flipCull, const bool renderFirstPersonShape = false);
 
    /// This enables terrain to be drawn inside interiors.
    void enableTerrainOverride();
+
+   /// Returns true if terrain is allowed to be drawn inside interiors.
+   bool isFirstPersonShapeOverridden() const;
+
+   /// This enables terrain to be drawn inside interiors.
+   void enableFirstPersonShapeOverride();
 
    /// Returns true if terrain is allowed to be drawn inside interiors.
    bool isTerrainOverridden() const;
@@ -361,6 +369,7 @@ class SceneState
    void renderNode(RenderBSPNode &rNode);
 
    bool mTerrainOverride;                             ///< If true, terrain is allowed to render inside interiors
+   bool mRenderFirstPersonShapeOverride;              ///< If true, First person control object is forced to render in this SceneState
 
    Vector<SceneState*>       mSubsidiaries;           ///< Transform portals which have been processed by the scene traversal process
                                                       ///
@@ -463,6 +472,16 @@ inline void SceneState::enableTerrainOverride()
 inline bool SceneState::isTerrainOverridden() const
 {
    return mTerrainOverride;
+}
+
+inline void SceneState::enableFirstPersonShapeOverride()
+{
+    mRenderFirstPersonShapeOverride = true;
+}
+
+inline bool SceneState::isFirstPersonShapeOverridden() const
+{
+    return mRenderFirstPersonShapeOverride;
 }
 
 

engine/sceneGraph/sceneTraversal.cc
@@ -276,7 +276,10 @@ void SceneGraph::buildSceneTree(SceneState*  state,
                                                smVisibleDistanceMod);
          newState->mFlipCull = state->mFlipCull ^ rPortal.flipCull;
          newState->setPortal(rPortal.owner, rPortal.portalIndex);
-
+         
+         // Don't immediately assume a portal is a mirror, the portal must tell us if we need to render the local player.
+         if (rPortal.renderFirstPersonShape)
+            newState->enableFirstPersonShapeOverride();
 
          glMatrixMode(GL_MODELVIEW);
          glPushMatrix();

commit 1066be8e2cbfd66f9cd8f82f34f66e90cebb12d3

Player: fix isFirstPerson check for freelook
Regression introduced by commit 4b9d0c5

engine/game/player.cc
@@ -1500,7 +1500,7 @@ void Player::updateMove(const Move* move)
       if (y > M_PI) y -= M_2PI;
 
       GameConnection* con = getControllingClient();
-      if (move->freeLook && ((isMounted() && getMountNode() == 0) || (con && !isFirstPerson())))
+      if (move->freeLook && ((isMounted() && getMountNode() == 0) || (con && !con->isFirstPerson())))
       {
          mHead.z = mClampF(mHead.z + y,
                            -mDataBlock->maxFreelookAngle,