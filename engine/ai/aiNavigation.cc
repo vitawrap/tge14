f97ed7258a82a9bd08153e56f2c9870cd46af950
vitawrap <thevitawrap@gmail.com>
2024-08-02 17:03:19 +0200

    AINavigation: add incomplete rainfall Init

--- engine/ai/aiNavigation.cc
+++ engine/ai/aiNavigation.cc
@@ -37,6 +37,16 @@ void AINavigation::onRemove()
 	Parent::onRemove();
 }
 
+void AINavigation::trashPathCache()
+{
+
+}
+
+void AINavigation::reseed(S32 seed)
+{
+	mRandom.setSeed(seed);
+}
+
 void AINavigation::mapFloors()
 {
 	/*
@@ -47,4 +57,31 @@ void AINavigation::mapFloors()
 		return;
 
 	*/
+}
+
+void AINavigation::rainfallInit(Box3F const& area, bool penetration, U32 typeMask, F32 raysPerUnit)
+{
+	/*
+	* We cast rays vertically from the top of the area to the bottom.
+	* Each intersection (if the type is accepted) is added as an AI node.
+	* Default density (rays per unit) is 0.125 so roughly one ray every 8 units.
+	*/
+
+	if (!gServerSceneGraph)
+		return;
+	
+	U32 rayCount = raysPerUnit * area.getVolume();
+
+	for (U32 i = 0; i < rayCount; ++i)
+	{
+		F32 xPos = mRandom.randF() * area.len_x();
+		F32 yPos = mRandom.randF() * area.len_y();
+		F32 zOrigin = area.max.z;
+
+		RayInfo rayInfo{};
+		while (gServerContainer.castRay(Point3F(xPos, yPos, zOrigin), Point3F(xPos, yPos, area.min.z), typeMask, &rayInfo))
+		{
+			zOrigin = rayInfo.point.z - POINT_EPSILON;
+		}
+	}
 }
--- engine/ai/aiNavigation.h
+++ engine/ai/aiNavigation.h
@@ -9,17 +9,40 @@
 #ifndef _SIMBASE_H_
 #include "console/simBase.h"
 #endif
+#ifndef _SIMPATH_H_
+#include "sim/simPath.h"
+#endif
+#ifndef _PATHMANAGER_H_
+#include "sim/pathManager.h"
+#endif
 
 /**
  * Not quite Tribes 2's AI graph navmesh system or whatever...
  * But a small module to aid in pathfinding and navigation.
+ * 
+ * This stores the adjacency lists of the paths it registers.
+ * 
+ * To use this structure optimally, an initialization sequence
+ * in script would have to, upon initializing a mission:
+ * * Trash cache
+ * * Look for mission path groups (from path manager)
+ *   * add them if any
+ * * Look for mission AI zones (not programmed yet)
+ *   * perform rainfall initialization on them
+ *   * rainfall density can be adjusted in the zone
+ * * Finish mission loading
  */
 class AINavigation : public SimObject
 {
 private:
 	typedef SimObject Parent;
 
+	// Randomizer used for this object
+	MRandomLCG mRandom;
+
 protected:
+	// When path nodes are changed, we must destroy our cache...
+	void trashPathCache();
 
 public:
 	AINavigation();
@@ -28,8 +51,28 @@ public:
 	virtual bool onAdd() override;
 	virtual void onRemove() override;
 
+	// Change random seed
+	void reseed(S32 seed);
+
 	// High-level function
 	void mapFloors();
+
+	// Return available path manager
+	static PathManager* getPathManager()
+	{
+		if (gServerPathManager)
+			return gServerPathManager;
+		return gClientPathManager;
+	}
+
+	// Simple pathfinding (shortest path)
+	void findPath(PathNode const* node);
+
+	// Pathfinding with respect to weights (shortest path)
+	void findMinPath(PathNode const* node);
+
+	// Perform rainfall initialization, (Penetration is whether or not we still extend the ray past intersections)
+	void rainfallInit(Box3F const& area, bool penetration = true, U32 typeMask = STATIC_COLLISION_MASK, F32 density = 0.125);
 
 	static void initPersistFields();
 	DECLARE_CONOBJECT(AINavigation);
