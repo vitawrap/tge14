2a8940e7248f28a70e96c296c5a7796932b145ff
vitawrap <thevitawrap@gmail.com>
2026-05-03 15:21:43 +0200

    Unicode: Fix string to UTF8 truncation

--- engine/core/unicode.h
+++ engine/core/unicode.h
@@ -75,6 +75,8 @@ const UTF32  oneUTF8toUTF32( const UTF8 *codepoint,  U32 *unitsWalked = NULL);
 const UTF32  oneUTF16toUTF32(const UTF16 *codepoint, U32 *unitsWalked = NULL);
 const UTF16  oneUTF32toUTF16(const UTF32 codepoint);
 const U32    oneUTF32toUTF8( const UTF32 codepoint, UTF8 *threeByteCodeunitBuf);
+void         oneUTF32toUTF8( const UTF32 codepoint, U32 bytecount, UTF8* threeByteCodeunitBuf);
+const U32	 oneUTF32length( UTF32& codepoint);
 
 //-----------------------------------------------------------------------------
 /// Functions that calculate the length of unicode strings.
